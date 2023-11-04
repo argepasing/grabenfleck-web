901b57522df75da5cd80d3af615e5ac7e251df30
neumayr <neumayr@users.noreply.github.com>
2023-11-04 14:48:47 +0100

    update astro to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import remarkDirective from "remark-directive";
+import m2dx from "astro-m2dx";
+
+/** @type {import('astro-m2dx').Options} */
+const m2dxOptions = {
+  styleDirectives: true,
+};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  markdown: {
+    remarkPlugins: [
+      remarkDirective, // required for styleDirectives
+      [m2dx, m2dxOptions],
+    ],
+    extendDefaultPlugins: true,
+  },
   vite: {
     define: {
-      "import.meta.env.PACKAGE_VERSION": JSON.stringify(`v${process.env.npm_package_version}`)
-    }
-  }
+      "import.meta.env.PACKAGE_VERSION": JSON.stringify(`v${process.env.npm_package_version}`),
+    },
+  },
 });
